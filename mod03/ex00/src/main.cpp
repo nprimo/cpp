fddbded15ba9b8305fe8e49cd0467a884eb53576
nprimo <primo.niccolo@gmail.com>
2022-11-01 12:29:33 +0000

    feat(mod03/ex00): update functions

--- mod03/ex00/src/main.cpp
+++ mod03/ex00/src/main.cpp
@@ -3,10 +3,18 @@
 int main(void)
 {
 	ClapTrap a ("mario");
+	ClapTrap c (a);
 	ClapTrap b;
 
 	a.attack("luigi");
 	b.takeDamage(2);
 	a.beRepaired(3);
+	b = c;
+	for (int i = 0; i < 10; i++)
+	{
+		a.attack("someone else");
+		c.attack("a tiger");
+	}
+	b.attack("nobody");
 	return (0);
 }
