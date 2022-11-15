0251af3f2f525284da2b19d73a149610ced40672
Niccolo Primo <primo.niccolo@gmail.com>
2022-11-15 17:45:54 +0000

    feat(mod08/ex02): first working version

--- mod08/ex02/inc/MutantStack.hpp
+++ mod08/ex02/inc/MutantStack.hpp
@@ -2,13 +2,34 @@
 # define MUTANTSTACK_H
 
 # include <stack>
-# include <iostream>
 
 template< typename T >
 class MutantStack : public std::stack< T > {
     private:
 
     public:
+		MutantStack() {}
+		MutantStack(const MutantStack &other) {
+			*this = other;
+		}
+		~MutantStack() {}
+		MutantStack &operator=(const MutantStack &other) {
+			this->std::stack< T >::operator=(other);
+			return *this;
+		}
+		typedef typename std::stack< T >::container_type::iterator iterator;
+		iterator begin() {
+			return std::stack< T >::c.begin();
+		}
+		iterator end() {
+			return std::stack< T >::c.end();
+		}
+		iterator cbegin() {
+			return std::stack< T >::c.cbegin();
+		}
+		iterator cend() {
+			return std::stack< T >::c.cend();
+		}
 };
 
 #endif
--- mod08/ex02/src/main.cpp
+++ mod08/ex02/src/main.cpp
@@ -1,26 +1,47 @@
 #include "../inc/MutantStack.hpp"
-#include <cstdlib>
-#include <stdlib.h>
+# include <iostream>
 
 #define SIZE 10
 
-int main() {
-    MutantStack<int> mstack;
-    MutantStack<int> mstack_copy;
-    
-    srand(time(NULL));
-    mstack.push(5);
-    mstack.push(17);
-    std::cout << mstack.top() << std::endl;
-    mstack.pop();
-    std::cout << mstack.size() << std::endl;
-    for (unsigned i = 0; i < SIZE; i++) {
-        mstack.push(rand() % 100);
-    }
-    for (unsigned i = 0; i < SIZE; i++) {
-        std::cout << mstack.top() << std::endl;
-        int top = mstack.top();
-        mstack.pop();
-        mstack.push(top);
-    }
+int main()
+{
+	MutantStack<int> mstack;
+
+
+	mstack.push(5);
+	mstack.push(17);
+	// std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	// std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+
+	std::cout << "copy mstack: " << std::endl;
+	MutantStack<int> mstack_cp (mstack);
+	MutantStack<int>::iterator it_cp = mstack_cp.begin();
+	MutantStack<int>::iterator ite_cp = mstack_cp.end();
+	while (it_cp != ite_cp) {
+		std::cout << *it_cp << " ";
+		it_cp++;
+	}
+	std::cout << std::endl;
+	while(!mstack_cp.empty()) {
+		mstack_cp.pop();
+	}
+	
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	std::cout << "mstack: " << std::endl;
+	while (it != ite) {
+		std::cout << *it << " ";
+		++it;
+	}
+	std::cout << std::endl;
+
+	return 0;
 }
